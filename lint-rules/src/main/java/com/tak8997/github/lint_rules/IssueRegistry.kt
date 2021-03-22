ba84edffcb76f2e6a3e17c4149b118a1306ad6e6
kyle lee <kyle.lee@balancehero.com>
2021-03-22 18:22:45 +0900

    Configure custom lint

---
+++ lint-rules/src/main/java/com/tak8997/github/lint_rules/IssueRegistry.kt
@@ -0,0 +1,13 @@
+package com.tak8997.github.lint_rules
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+
+class IssueRegistry : IssueRegistry() {
+
+    override val api: Int = CURRENT_API
+
+    override val issues: List<Issue>
+        get() = listOf()
+}
