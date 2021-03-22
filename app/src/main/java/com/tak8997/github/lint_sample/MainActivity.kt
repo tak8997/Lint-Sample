b277965f37e8d865b729b8f8db884a2b3faef85e
kyle lee <kyle.lee@balancehero.com>
2021-03-22 18:01:59 +0900

    initial commit

---
+++ app/src/main/java/com/tak8997/github/lint_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tak8997.github.lint_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
